a00bff2d36d48e68ad0af533d067c3ed44887083
LukeV511 <lucasveglia7@gmail.com>
2024-07-05 15:23:20 +0000

    initial commit

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@ AnalogIn CurrentSensor(A1);
 
 UnbufferedSerial uartUsb(USBTX, USBRX, 9600);
 
-float vsensitivity = 1/125.2;
-float isensitivity = 0.185;
+float vsensitivity = 1/110.5;
+float isensitivity = 0.2;
 
 //main code
 
@@ -18,16 +18,17 @@ int main()
         float voltage = VoltageSensor.read();
         float current = CurrentSensor.read();
 
-        float Voltage = ((voltage * 5.000/1023.000)-2.5)/vsensitivity; 
-        float Current = ((current * 5.000/1023.000)-2.5)/isensitivity;
+        float Voltage = (voltage-0.606)/vsensitivity; 
+        float Current = (current-0.5)/isensitivity;
 
         float Power = Voltage * Current;
         
         char str[100];
-        sprintf(str, "Voltage: %.2f V \r\n", Voltage);
+        sprintf(str, "Voltaje: %.2f V, Corriente: %.2f A, Potencia: %.2f W \r\n", Voltage, Current, Power);
         
         uartUsb.write(str, strlen(str));
         
-        ThisThread::sleep_for(500ms);
+        ThisThread::sleep_for(1ms);
+
     }
 }
